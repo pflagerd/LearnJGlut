8bfd0d0c866de148db46c5130dd93c77a5b4ee13
karthik vs <karthik.venkatarayanadoddisuresh@boeing.com>
2019-04-08 04:34:41 -0700

    Java_com_pflager_gl_glVertex2f__DD
    Former-commit-id: c99c48ffec1552e4c29f939cbf48bc6647e8a2eb

---
+++ src/com/pflager/Java_com_pflager_gl_glVertex2f__DD.java
@@ -0,0 +1,14 @@
+package com.pflager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Java_com_pflager_gl_glVertex2f__DD {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
