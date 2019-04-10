45b856fb5381c3c5032601beff6a5af8fb3d202d
Administrator <Administrator@A5920902-12R2A.nos.boeing.com>
2019-04-10 02:07:45 -0700

    glRecti committed
    Former-commit-id: 740382cd61e8da66ff0a8ca73c4fb68ed9ac6b1d

--- src/com/pflager/Test_com_pflager_gl_glRecti.java
+++ src/com/pflager/Test_com_pflager_gl_glRecti.java
@@ -11,9 +11,8 @@ public class Test_com_pflager_gl_glRecti extends glutTest{
 		singleShotDisplayTest(() -> {
 			glClear(GL_COLOR_BUFFER_BIT);
 			//glOrtho(0.0, 10.0, 0.0, 10.0, 0.0, 10.0);
-
-			glRecti(-1, -1, 1, 1);
 			
+			glRecti(-1, -1, 1, 1);			
 
 			glFinish(); // waits for display to settle down.
 
