605838c36958151e8e33c7db20e21b7e533be86f
karthik vs <karthik.venkatarayanadoddisuresh@boeing.com>
2019-04-10 19:12:56 -0700

    Updating Test case
    Former-commit-id: 332c80a47b7c73e5559507b8d4b9d0f2075bd54a

--- src/com/pflager/Test_com_pflager_gl_glVertex2fv__3F.java
+++ src/com/pflager/Test_com_pflager_gl_glVertex2fv__3F.java
@@ -10,7 +10,7 @@ import java.lang.Math;
 class Test_com_pflager_gl_glVertex2fv__3F extends glutTest {
 
 	@Test
-	void testDrawSpiralStartingAtTheExactCenter_II_II() throws IOException, InterruptedException {
+	void testDrawSpiralStartingAtTheExactCenter_FF() throws IOException, InterruptedException {
 		singleShotDisplayTest(() -> {
 			glClear(GL_COLOR_BUFFER_BIT);
 			final int circle_points = 256;
@@ -24,14 +24,11 @@ class Test_com_pflager_gl_glVertex2fv__3F extends glutTest {
 					IncrementalRadius = IncrementalRadius +  (1/ (double)(circle_points*circle_count));
 					k = k + 360 / (double) circle_points; //Increments Spacing between points
 			    	glColor3f(0f, 0f, 1f);
-					glVertex2f(Math.cos(Math.toRadians(k)) * IncrementalRadius, Math.sin(Math.toRadians(k)) * IncrementalRadius);
+					glVertex2f((float)Math.cos(Math.toRadians(k)) * IncrementalRadius, (float)Math.sin(Math.toRadians(k)) * IncrementalRadius);
 				}
 			} 
 			glEnd();
 			glFinish(); // waits for display to settle down.
-			
-			
-			
 			
 			try {
 				captureCanvasAsImageFile("artifacts/tmp.png");
@@ -42,5 +39,69 @@ class Test_com_pflager_gl_glVertex2fv__3F extends glutTest {
 			glutLeaveMainLoop();
 		});
 	}
-
+	
+	@Test
+	void testDrawSpiralStartingAtTheExactCenter_DD() throws IOException, InterruptedException {
+		singleShotDisplayTest(() -> {
+			glClear(GL_COLOR_BUFFER_BIT);
+			final int circle_points = 256;
+			final int circle_count = 9;
+			final double circle_spacing = (1/(double)circle_count);
+			glBegin(GL_POINTS);
+			double IncrementalRadius = 0 ; 
+			for (double radius = circle_spacing; (radius < 1.1); radius = ( radius +circle_spacing)) {
+				double k = 0;
+				for (double i = 0; i < circle_points; i++) { 
+					IncrementalRadius = IncrementalRadius +  (1/ (double)(circle_points*circle_count));
+					k = k + 360 / (double) circle_points; //Increments Spacing between points
+			    	glColor3f(0f, 0f, 1f);
+					glVertex2f((float)Math.cos(Math.toRadians(k)) * IncrementalRadius, (float)Math.sin(Math.toRadians(k)) * IncrementalRadius);
+				}
+			} 
+			glEnd();
+			glFinish(); // waits for display to settle down.
+			
+			try {
+				captureCanvasAsImageFile("artifacts/tmp.png");
+			} catch (IOException ioException) {
+				System.err.println("Couldn't save file artifacts/tmp.png");
+				ioException.printStackTrace(System.err);
+			}
+			glutLeaveMainLoop();
+		});
+	}
+	
+	@Test
+	void testDrawSpiralStartingAtTheExactCenter_DF() throws IOException, InterruptedException {
+		singleShotDisplayTest(() -> {
+			glClear(GL_COLOR_BUFFER_BIT);
+			final int circle_points = 256;
+			final int circle_count = 9;
+			final double circle_spacing = (1/(double)circle_count);
+			glBegin(GL_POINTS);
+			double IncrementalRadius = 0 ; 
+			for (double radius = circle_spacing; (radius < 1.1); radius = ( radius +circle_spacing)) {
+				double k = 0;
+				for (double i = 0; i < circle_points; i++) { 
+					IncrementalRadius = IncrementalRadius +  (1/ (double)(circle_points*circle_count));
+					k = k + 360 / (double) circle_points; //Increments Spacing between points
+			    	glColor3f(0f, 0f, 1f);
+					glVertex2f(Math.cos(Math.toRadians(k)) * IncrementalRadius, (float)Math.sin(Math.toRadians(k)) * IncrementalRadius);
+				}
+			} 
+			glEnd();
+			glFinish(); // waits for display to settle down.
+			
+			try {
+				captureCanvasAsImageFile("artifacts/tmp.png");
+			} catch (IOException ioException) {
+				System.err.println("Couldn't save file artifacts/tmp.png");
+				ioException.printStackTrace(System.err);
+			}
+			glutLeaveMainLoop();
+		});
+	}
+	
+	
+	
 }
