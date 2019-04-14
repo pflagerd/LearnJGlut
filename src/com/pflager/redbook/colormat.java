59a5d81deace6d1212ad42297ab4fe7b88a4f1bd
karthik vs <karthik.venkatarayanadoddisuresh@boeing.com>
2019-04-14 03:39:08 -0700

    Updated
    Former-commit-id: 744d0825cba53606e33efcf21eaf4c989421bc7a

--- src/com/pflager/redbook/colormat.java
+++ src/com/pflager/redbook/colormat.java
@@ -1,5 +1,100 @@
 package com.pflager.redbook;
+import com.pflager.glut;
+public class colormat  extends glut{
+	float diffuseMaterial[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+	void init() 
+	{
+		float mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+		float light_position[] = { 1.0f, 1.0f, 1.0f, 0.0f };
+		glClearColor (0.0, 0.0, 0.0, 0.0);
+		glShadeModel (GL_SMOOTH);
+		glEnable(GL_DEPTH_TEST);
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuseMaterial);
+		glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+		glMaterialf(GL_FRONT, GL_SHININESS, 25.0);
+		glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+		glEnable(GL_LIGHTING);
+		glEnable(GL_LIGHT0);
+		glColorMaterial(GL_FRONT, GL_DIFFUSE);
+		glEnable(GL_COLOR_MATERIAL);
+	}
+	
+	
 
-public class colormat {
+	void mouse(int button, int state, int x, int y) {
+		int viewport[] = new int[4];
+		double mvmatrix[] = new double[16];
+		double projmatrix[] = new double[16];
+		int realy; /* OpenGL y coordinate position */
+		switch (button) {
+			case GLUT_LEFT_BUTTON:
+				if (state == GLUT_DOWN) {
+					diffuseMaterial[0] += 0.1;
+					if (diffuseMaterial[0] > 1.0)
+					diffuseMaterial[0] =(float) 0.0;
+					glColor4fv(diffuseMaterial);
+					glutPostRedisplay();
+				}
+				break;
+			case GLUT_RIGHT_BUTTON:
+				if (state == GLUT_DOWN)
+					diffuseMaterial[2] += 0.1;
+					if (diffuseMaterial[2] > 1.0)
+						diffuseMaterial[2] = (float) 0.0;
+						glColor4fv(diffuseMaterial);
+						glutPostRedisplay();
+				break;
+			default:
+				break;
+		}
+	}
 
+	void display()
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glutSolidSphere(1.0, 20, 16);
+		glFlush ();
+	}
+
+	void reshape (int w, int h)
+	{
+		glViewport (0, 0, w,h);
+		glMatrixMode (GL_PROJECTION);
+		glLoadIdentity();
+		if (w <= h)
+		glOrtho (-1.5, 1.5, -1.5*h/w,
+		1.5*h/w, -10.0, 10.0);
+		else
+		glOrtho (-1.5*w/h,1.5*w/h, -1.5, 1.5, -10.0, 10.0);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+	}
+
+	void keyboard( char key, int x, int y)
+	{
+	   switch (key) {
+	      case 27:
+	        System. exit(0);
+	         break;
+	   }
+	}
+
+	public int main(int argc, String[] argv)
+	{
+	   glutInit(argc, argv);
+	   glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	   glutInitWindowSize (500, 500); 
+	   glutInitWindowPosition (100, 100);
+	   glutCreateWindow ("colormat");
+	   init ();
+	   glutDisplayFunc(this::display); 
+	   glutReshapeFunc(this::reshape);
+	   glutKeyboardFunc(this::keyboard);
+	   glutMouseFunc(this::mouse);
+	   glutMainLoop();
+	   return 0;
+	}
+	public static void main(String[] args) {
+		System.exit(new colormat().main(args.length, args));
+	}
 }
