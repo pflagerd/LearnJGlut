f8567332b7e9685bfa5ba2052b469baa48b876c3
karthik vs <karthik.venkatarayanadoddisuresh@boeing.com>
2019-04-15 02:12:42 -0700

    Updating movelight
    Former-commit-id: f76fce4018f26f49c6cf09ac8e8b59a0ba828347

--- src/com/pflager/redbook/movelight.java
+++ src/com/pflager/redbook/movelight.java
@@ -13,6 +13,7 @@ public class movelight extends glut {
 		glEnable(GL_DEPTH_TEST);
 	}
 
+	
 	void display() {
 		float position[] = { 0.0f, 0.0f, 1.5f, 1.0f };
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
