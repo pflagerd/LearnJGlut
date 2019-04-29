d5f507a9f3ab6dceeb2bfbcd37f048e95d1fd815
Daniel P Pflager <daniel.pflager@gmail.com>
2019-04-28 17:07:38 -0700

    progress
    Former-commit-id: 5b82ee4ddebb215bd3957e87df74a5a7bb47d3f2 [formerly 2c552065dafec3de2960e977f285a5cc45e1f8b5]
    Former-commit-id: 30c16f2afc3bd3a84de5c04c153930fe481a02cf

--- redbook-1.1-src/src/build-windows.sh
+++ redbook-1.1-src/src/build-windows.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function build() {
-../../x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/bin/gcc -DFREEGLUT_STATIC -I../../freeglut/include $1.c -o $1 -L../../freeglut/lib/x64 -lfreeglut_static -lopengl32 -lglu32 -lwinmm -lgdi32 -Wl,--subsystem,windows
+	[ $1.c -nt $1 ] && ../../x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/bin/gcc -DFREEGLUT_STATIC -I../../freeglut/include $1.c -o $1 -L../../freeglut/lib/x64 -lfreeglut_static -lopengl32 -lglu32 -lwinmm -lgdi32 -Wl,--subsystem,windows
 }
 
 for program in `ls -1 *.c`; do
