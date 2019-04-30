584e0f50faeb4d16c6ee5a55d0fbdcd02a13fd2a
karthik vs <karthik.venkatarayanadoddisuresh@boeing.com>
2019-04-30 03:45:52 -0700

    Update
    Former-commit-id: 85bfcd54272566c64e87f685bef82f1e508dabcc [formerly 9f7988ac27abab2e6470b407431c666d24f09bba]
    Former-commit-id: bd9b37b19613f73b87551abc2b659fea58c2437a

--- src/com/pflager/redbook/Test_com_pflager_redbook_hello.java
+++ src/com/pflager/redbook/Test_com_pflager_redbook_hello.java
@@ -4,19 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class Test_com_pflager_redbook_hello  extends ImageCompareJNA{
-
+class Test_com_pflager_redbook_hello extends ImageCompareJNA {
 
 	@Test
 	void test() {
 		assertTrue(CaptureCImage("hello"));
 		new Thread(() -> {
-			hello helloObject = new hello() ;
+			hello helloObject = new hello();
 			String argv[] = new String[0];
 			helloObject.main(0, argv);
-			}).start();
+		}).start();
 		assertTrue(CompareImageSec("hello"));
-		
+
 	}
 
 }
