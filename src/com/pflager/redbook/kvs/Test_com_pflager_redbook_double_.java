c953f452df0f73fae97b5e17c88ab58cf36e1013
karthik vs <karthik.venkatarayanadoddisuresh@boeing.com>
2019-05-12 10:38:10 -0700

    Update
    Former-commit-id: 2a7d1e42909310db0e7ba83ed60cdf258fac6f1c [formerly 5831b8dd45bf02f63e18bc92d52b49b395c0ef14]
    Former-commit-id: ff1ac9af1ba2c82615d873c41839a91e8b8c681b

--- src/com/pflager/redbook/kvs/Test_com_pflager_redbook_double_.java
+++ src/com/pflager/redbook/kvs/Test_com_pflager_redbook_double_.java
@@ -10,9 +10,9 @@ class Test_com_pflager_redbook_double_ extends ImageCompareJNA {
 
 	@Test
 	void test() throws Throwable {
-		assertTrue(CaptureCImage("double_"));
+		assertTrue(CaptureCImage("double"));
 		RunNewProcess(double_.class.getName());
-		boolean CompareImage = CompareImageSec("double_");
+		boolean CompareImage = CompareImageSec("double");
 		super.finalize();
 		assertTrue(CompareImage);
 	}
