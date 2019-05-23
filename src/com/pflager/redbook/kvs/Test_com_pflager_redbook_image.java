273baa7737c930bad90dec68c3a085041b155728
karthik vs <karthik.venkatarayanadoddisuresh@boeing.com>
2019-05-22 22:08:51 -0700

    Implement image.java
    Former-commit-id: 5f2b5167a43f82b3a985b9174e95fe109de5f716
    Former-commit-id: db7b37e44f9c5ca14fa86f27cfd24123af1c34ae

--- src/com/pflager/redbook/kvs/Test_com_pflager_redbook_image.java
+++ src/com/pflager/redbook/kvs/Test_com_pflager_redbook_image.java
@@ -3,7 +3,8 @@ package com.pflager.redbook.kvs;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-class Test_com_pflager_redbook_image  extends ImageCompareJNA {
+
+class Test_com_pflager_redbook_image extends ImageCompareJNA {
 	@Test
 	void test() throws Throwable {
 		assertTrue(CaptureCImage("image"));
