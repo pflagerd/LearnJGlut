93f20e43438e565d924343df3f7a8fe30a30163e
Vishnu <vishnutholasireddy.gosupati@boeing.com>
2019-05-27 23:07:40 -0700

    WIP
    Former-commit-id: 06c57e76f5b5c37b155012e0de29b5698d2f4de5
    Former-commit-id: d89e6d95b4269df164b65eff84353ce2f3ff652e

--- src/com/pflager/redbook/vtrg/checker.java
+++ src/com/pflager/redbook/vtrg/checker.java
@@ -32,16 +32,15 @@ public class checker extends glut{
 		glClearColor (0.0, 0.0, 0.0, 0.0);
 		glShadeModel(GL_FLAT);
 		glEnable(GL_DEPTH_TEST);
+		//glDepthFunc(GL_LESS);
 		makeCheckImage();
 		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 		glGenTextures(1,new int[] {texName});
 		glBindTexture(GL_TEXTURE_2D, texName);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
-		GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
-		GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, checkImageWidth,checkImageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE,checkImage);
 		
 	}
