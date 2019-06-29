ef8a2eb5c228ac4d6e16ca29dbe08296edc87d13
Daniel P Pflager <daniel.pflager@gmail.com>
2019-06-28 19:57:10 -0700

    gluNurbsCallback()
    Former-commit-id: 10d886d3c7f4e902eb22da50d94fe79e99285563
    Former-commit-id: 5f10ed74645d77c7a654c07d2002cdffaaed9853

--- src/org/pflager/redbook/dpp/surface.java
+++ src/org/pflager/redbook/dpp/surface.java
@@ -101,7 +101,7 @@ public class surface extends glut {
 	   theNurb = gluNewNurbsRenderer();
 	   gluNurbsProperty(theNurb, GLU_SAMPLING_TOLERANCE, 25.0);
 	   gluNurbsProperty(theNurb, GLU_DISPLAY_MODE, GLU_FILL);
-	   gluNurbsCallback(theNurb, GLU_ERROR, nurbsError);
+	   gluNurbsCallback(theNurb, GLU_ERROR, this::nurbsError);
 	}
 
 	void display()
