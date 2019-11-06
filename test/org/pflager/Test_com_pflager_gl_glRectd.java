cf5c86d4a9c2cdc913c1cdb79043e8c83b65648d
Daniel P Pflager <daniel.pflager@gmail.com>
2019-11-05 23:56:19 -0800

    formatting.
    Former-commit-id: 22ebc8d957b12489d2823cfca70447a0a39d351a

--- test/org/pflager/Test_com_pflager_gl_glRectd.java
+++ test/org/pflager/Test_com_pflager_gl_glRectd.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
-public class Test_com_pflager_gl_glRectd extends glutTest{
+public class Test_com_pflager_gl_glRectd extends glutTest {
 
 	@Test
 	void testFullyCoveredCanvas_DD_DD() throws IOException, InterruptedException {
 		singleShotDisplayTest(() -> {
 			glClear(GL_COLOR_BUFFER_BIT);
-			
-			glRectd(-0.001,-0.001,0.001,0.001);
+
+			glRectd(-0.001, -0.001, 0.001, 0.001);
 
 			glFinish(); // waits for display to settle down.
 
@@ -24,12 +24,12 @@ public class Test_com_pflager_gl_glRectd extends glutTest{
 			glutLeaveMainLoop();
 		});
 	}
-	
+
 	@Test
 	void testSquareInTheMiddleOfTheCanvas_DD_DD() throws IOException, InterruptedException {
 		singleShotDisplayTest(() -> {
 			glClear(GL_COLOR_BUFFER_BIT);
-			glRectd(-0.07, -0.07, 0.007,0.007);
+			glRectd(-0.07, -0.07, 0.007, 0.007);
 			glFinish(); // waits for display to settle down.
 
 			try {
@@ -41,12 +41,12 @@ public class Test_com_pflager_gl_glRectd extends glutTest{
 			glutLeaveMainLoop();
 		});
 	}
-	
+
 	@Test
 	void testPartiallyCoveredCanvas_DD_DD() throws IOException, InterruptedException {
 		singleShotDisplayTest(() -> {
 			glClear(GL_COLOR_BUFFER_BIT);
-			//glRectd(-3, -3, -0.01, 0.01);
+			// glRectd(-3, -3, -0.01, 0.01);
 			glRectd(-0.01, -0.1, -3, -3);
 			glFinish(); // waits for display to settle down.
 
