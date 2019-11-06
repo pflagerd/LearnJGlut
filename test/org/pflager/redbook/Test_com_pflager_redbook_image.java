33f2d1fed2d512b8c138554c5c2ae17213588551
Daniel P Pflager <daniel.pflager@gmail.com>
2019-11-05 17:02:16 -0800

    suppress silly warning.
    Former-commit-id: cbbe744678f563ebf9b010dfc8e6b14e542cfe5f

--- test/org/pflager/redbook/Test_com_pflager_redbook_image.java
+++ test/org/pflager/redbook/Test_com_pflager_redbook_image.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.pflager.redbook.kvs.image;
 
+@SuppressWarnings("serial")
 class Test_com_pflager_redbook_image extends ImageCompareJNA {
 	@Test
 	void test() throws Throwable {
